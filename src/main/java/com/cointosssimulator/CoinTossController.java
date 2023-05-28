952f4464a50efb044c78bccd90fdef5a9a5e3a39
Tbusk <buskt@ferris.edu>
2023-05-28 05:12:42 -0400

    Added random coin flip based on Random class to decide to decide which end the coin will land on using integers.  Also added fxml files for each result for dime, nickel, and quarter, which is heads and tails for each.  Once the user selects heads or tails, the scene changes to what the coin landed on with an image.

--- src/main/java/com/cointosssimulator/CoinTossController.java
+++ src/main/java/com/cointosssimulator/CoinTossController.java
@@ -17,6 +17,7 @@ import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class CoinTossController implements Initializable {
@@ -24,6 +25,10 @@ public class CoinTossController implements Initializable {
     private Scene mainScreen;
     private Stage mainStage;
     private Parent root;
+    static String coinSelection;
+
+    protected ArrayList<Integer> results = new ArrayList<>();
+    protected ArrayList<Integer> selection = new ArrayList<>();
 
     @FXML
     private Button startButton, backButton, headsButton, tailsButton;
@@ -43,6 +48,7 @@ public class CoinTossController implements Initializable {
             mainStage = (Stage)((Node)event.getSource()).getScene().getWindow();
             mainScreen = new Scene(root);
             mainScreen.getStylesheets().add(getClass().getResource("CoinTossStyle.css").toExternalForm());
+            coinSelection = coinList.getValue();
             mainStage.setScene(mainScreen);
         } catch (Exception e) {
             System.out.println(e);
@@ -62,17 +68,83 @@ public class CoinTossController implements Initializable {
     }
 
     public ObservableList<String> createCoinList() {
-        ObservableList<String> coins = FXCollections.observableArrayList("Quarter","Nickel","Dime", "Penny");
+        ObservableList<String> coins = FXCollections.observableArrayList("Quarter","Nickel","Dime");
         return coins;
+    }
+
+    @FXML
+    private void headsOrTails(ActionEvent event) {
+        Random random = new Random();
+        results.add(random.nextInt(2));
+        if(event.getSource()==tailsButton) {
+            selection.add(1);
+            System.out.println("User selected tails.");
+            if (results.get(results.size()-1) == 1) {
+                System.out.println("The coin landed on tails.");
+            } else {
+                System.out.println("The coin landed on heads.");
+            }
+
+        }
+        if(event.getSource()==headsButton) {
+            selection.add(0);
+            System.out.println("User selected heads.");
+            if (results.get(results.size()-1) == 1) {
+                System.out.println("The coin landed on tails.");
+            } else {
+                System.out.println("The coin landed on heads.");
+            }
+        }
+        if(coinSelection.equals("Quarter")){
+                try {
+                    if(results.get(results.size() - 1) == 0) { // Heads
+                        root = FXMLLoader.load(getClass().getResource("Quarter-heads.fxml"));
+                    } else {
+                        root = FXMLLoader.load(getClass().getResource("Quarter-tails.fxml"));
+                    }
+                    mainStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                    mainScreen = new Scene(root);
+                    mainScreen.getStylesheets().add(getClass().getResource("CoinTossStyle.css").toExternalForm());
+                    mainStage.setScene(mainScreen);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+        } else if(coinSelection.equals("Nickel")){
+            try {
+                if(results.get(results.size() - 1) == 0) { // Heads
+                    root = FXMLLoader.load(getClass().getResource("Nickel-heads.fxml"));
+                } else {
+                    root = FXMLLoader.load(getClass().getResource("Nickel-tails.fxml"));
+                }
+                mainStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                mainScreen = new Scene(root);
+                mainScreen.getStylesheets().add(getClass().getResource("CoinTossStyle.css").toExternalForm());
+                mainStage.setScene(mainScreen);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        } else if(coinSelection.equals("Dime")){
+            try {
+                if(results.get(results.size() - 1) == 0) { // Heads
+                    root = FXMLLoader.load(getClass().getResource("Dime-heads.fxml"));
+                } else {
+                    root = FXMLLoader.load(getClass().getResource("Dime-tails.fxml"));
+                }
+                mainStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                mainScreen = new Scene(root);
+                mainScreen.getStylesheets().add(getClass().getResource("CoinTossStyle.css").toExternalForm());
+                mainStage.setScene(mainScreen);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
     }
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
-            if(coinList.getItems() != null) {
                 coinList.getItems().addAll(createCoinList());
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
